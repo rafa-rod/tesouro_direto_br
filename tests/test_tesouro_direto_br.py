a80b70d548b4352bc541ccee72c79cbb08f54478
RaR <rafael.rodrigues@bndes.gov.br>
2025-02-01 11:51:55 -0300

    update dependencies contraints

--- tests/test_tesouro_direto_br.py
+++ tests/test_tesouro_direto_br.py
@@ -5,27 +5,25 @@ import matplotlib.pyplot as plt
 import comparar_fundos_br as comp
 import getpass
 
-import sys
-sys.path.append("./src/tesouro_direto_br")
-import tesouro_direto_br as tesouro_direto
+from src.tesouro_direto_br import *
 
 class TestClass():
 
     proxies = None
-    taxa_agrupada = tesouro_direto.busca_tesouro_direto(tipo="taxa", proxies=proxies, agrupar=True)
+    taxa_agrupada = busca_tesouro_direto(tipo="taxa", proxies=proxies, agrupar=True)
     assert not taxa_agrupada.empty
 
-    carteira = tesouro_direto.Carteira(tesouro_direto.Titulo())
-    titulo1 = tesouro_direto.Titulo("Tesouro IPCA+", "2026-08-15", "2021-07-08", 33.65)
-    titulo2 = tesouro_direto.Titulo("Tesouro Selic", "2025-03-01", "2021-07-08", 50)
+    carteira = Carteira(Titulo())
+    titulo1 = Titulo("Tesouro IPCA+", "2026-08-15", "2021-07-08", 33.65)
+    titulo2 = Titulo("Tesouro Selic", "2025-03-01", "2021-07-08", 50)
     carteira.add(titulo1)
     carteira.add(titulo2)
     assert len(carteira.titulos)==2
 
-    carteira_tesouro_direto = tesouro_direto.calcula_retorno_carteira(carteira, proxies=proxies)
+    carteira_tesouro_direto = calcula_retorno_carteira(carteira)
     assert not carteira_tesouro_direto.empty
 
-    movimentacao_pivot = tesouro_direto.movimentacoes_titulos_publicos("venda", proxies=proxies)
+    movimentacao_pivot = movimentacoes_titulos_publicos("venda", proxies=proxies)
     assert not movimentacao_pivot.empty
 
     ano, mes, dia = str(carteira_tesouro_direto.index[0]).split('-')
@@ -35,7 +33,7 @@ class TestClass():
     assert retorno_periodo != 0
 
     mtm_carteira = carteira_tesouro_direto[["MTM"]]
-    custos, detalhamento_custos = tesouro_direto.get_custos( mtm_carteira, custo_b3=True )
+    custos, detalhamento_custos = get_custos( mtm_carteira, custo_b3=True )
     assert custos > 0
 
 TestClass()
